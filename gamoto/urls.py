bb485e4de769edd658fcd7962ac66cadc9faaa4a
Colin Alston <colin.alston@gmail.com>
2019-04-16 17:22:06 +0100

    Big bang

--- gamoto/urls.py
+++ gamoto/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
+
+from gamoto import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('social_django.urls', namespace='social')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.index, name="index"),
 ]
