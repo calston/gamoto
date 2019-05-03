539f112084768f674f6466d504689a52d4ef9f03
Colin Alston <colin.alston@gmail.com>
2019-05-03 17:08:07 +0100

    Messing around with tests

--- gamoto/tests.py
+++ gamoto/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, exceptions
+from django.urls.resolvers import URLPattern
+
 
 # Create your tests here.
+class TestRockfaceCase(TestCase):
+    def test_authentication_required(self):
+        from gamoto import urls
+        
+        redirect_map = []
+        for url in urls.urlpatterns:
+            if not isinstance(url, URLPattern):
+                continue
+            try:
+                url = reverse(url.name)
+                redirect_map.append((url.name, url))
+            except exceptions.NoReverseMatch:
+                pass
+
+        print(redirect_map)
+        r = self.client.get(reverse('index'))
+        self.assertRedirects(r, reverse('login')+'?next=/')
+
+    def test_login(self):
+        self.client.login(username='test', password='test')
