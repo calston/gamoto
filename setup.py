de9ad2e96d9a6e6a304c3f45d8fe89032300fd0c
Colin Alston <colin.alston@gmail.com>
2019-05-13 11:37:18 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gamoto',
-    version='0.0.7',
+    version='0.0.8',
     url='http://github.com/calston/gamoto',
     description='A web portal for VPN self service',
     classifiers=[
