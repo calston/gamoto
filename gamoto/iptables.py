378dbe0d629658d2f5b848921dd4e94f9f1c9174
Colin Alston <colin.alston@gmail.com>
2019-05-17 16:32:17 +0100

    Moving iptables stuff into its own library

---
+++ gamoto/iptables.py
@@ -0,0 +1,120 @@
+from gamoto import users
+
+
+class IPTables(object):
+    def __init__(self):
+        self.tables = {
+            'filter': {
+                'INPUT': {'rules': [], 'default': None},
+                'OUTPUT': {'rules': [], 'default': None},
+                'FORWARD': {'rules': [], 'default': None}
+            },
+            'nat': {
+                'PREROUTING': {'rules': [], 'default': None},
+                'INPUT': {'rules': [], 'default': None},
+                'OUTPUT': {'rules': [], 'default': None},
+                'POSTROUTING': {'rules': [], 'default': None}
+            }
+        }
+
+        self.refreshTables()
+
+    def refreshTables(self):
+        result, err = users.sudo('iptables-save')
+        result = result.strip()
+
+        if err:
+            raise Exception(err)
+
+        rules = [i for i in result.decode().split('\n')
+                 if not i.startswith('#')]
+
+        for r in rules:
+            if r.startswith('*'):
+                table = r.strip('*')
+                if table not in self.tables:
+                    self.tables[table] = {}
+            elif r.startswith(':'):
+                chain, default, b = r.strip(':').split()
+                if default == '-':
+                    default = None
+
+                if chain not in self.tables[table]:
+                    self.tables[table][chain] = {
+                        'default': default,
+                        'rules': []
+                    }
+            elif r.startswith('-'):
+                token, chain, args = r.split(None, 2)
+                self.tables[table][chain]['rules'].append(args)
+
+    def iptables(self, rule):
+        result, err = users.sudo('/sbin/iptables', *rule)
+        if err:
+            raise Exception(err)
+
+    def getTable(self, table):
+        return self.table.get(table, {})
+
+    def getChain(self, table, chain):
+        return self.table.get(table, {}).get(chain, {})
+
+    def createChain(self, table, chain):
+        if chain not in self.getTable(table):
+            self.iptables('-t', table, '-N', chain)
+
+    def addRule(self, table, chain, match, action, comment=None, top=False):
+        rules = self.getChain(table, chain).get('rules', [])
+
+        rtest = [match, '-j', action]
+        if comment:
+            rtest.append('-m comment --comment', '"%s"' % comment)
+
+        if ' '.join(rtest) in rules:
+            return
+
+        rule = ['-t', table]
+
+        if top:
+            rule.extend(['-I', chain, '1'])
+        else:
+            rule.extend(['-A', chain])
+
+        rule.extend([match, '-j', action])
+
+        if comment:
+            rule.extend([, '-m', 'comment', '--comment', comment])
+
+        self.iptables(rule)
+
+    def setupIptables(self):
+        # Makes sure iptables has our chain
+        self.refreshTables()
+
+        self.createChain('filter', 'openvpn')
+
+        input_rules = self.getChain('filter', 'INPUT').get('rules', [])
+
+        self.addRule('filter', 'INPUT', '-i ' + settings.VPN_INTERFACE,
+                     'openvpn', top=True)
+
+        if settings.DEFAULT_DENY:
+            self.addRule('filter', 'INPUT', '-i ' + settings.VPN_INTERFACE,
+                         'DROP')
+
+    def allowClient(self, name, src, dest):
+        self.refreshTables()
+        if not (name and src and dest):
+            return None
+
+        rule = '-i %s -s %s -d %s ' % (self.VPN_INTERFACE, src, dest)
+
+        self.addRule('filter', 'INPUT', rule, 'ACCEPT', comment=name)
+
+    def flushClient(self, name):
+        self.refreshTables()
+        rules = self.getChain('filter', 'openvpn').get('rules', [])
+
+        for i, r in enumerate(rules):
+            if name in r:
+                self.iptables('-D openvpn %s' % (i + 1))
--- gamoto/management/commands/openvpn.py
+++ gamoto/management/commands/openvpn.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.contrib.auth.models import User
 
-from gamoto import users, openvpn
+from gamoto import users, openvpn, iptables
 
 import os
 import sys
@@ -14,71 +14,6 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('client_cmd_path', nargs='?', type=str)
-
-    def getIptables(self):
-        result, err = users.sudo('iptables-save')
-        result = result.strip()
-        rules = [i for i in result.decode().split('\n')
-                 if not i.startswith('#')]
-
-        tables = {}
-        table = None
-        for r in rules:
-            if r.startswith('*'):
-                table = r.strip('*')
-                tables[table] = {}
-            elif r.startswith(':'):
-                chain, default, b = r.strip(':').split()
-                if default == '-':
-                    default = None
-
-                tables[table][chain] = {
-                    'default': default,
-                    'rules': []
-                }
-            elif r.startswith('-'):
-                token, chain, args = r.split(None, 2)
-                tables[table][chain]['rules'].append(args)
-        return tables
-
-    def iptables(self, rule):
-        result, err = users.sudo('/sbin/iptables', *rule.split())
-        if err:
-            raise Exception(err)
-
-    def setupIptables(self):
-        # Makes sure iptables has our chain
-        ipt = self.getIptables()
-        if 'openvpn' not in ipt.get('filter', {}):
-            self.iptables('-N openvpn')
-
-        jump_rule = '-i %s -j openvpn' % settings.VPN_INTERFACE
-
-        input_rules = ipt.get('filter', {}).get('INPUT', {}).get('rules', [])
-
-        if jump_rule not in input_rules:
-            self.iptables('-I INPUT 1 -i tun0 -j openvpn')
-
-    def allowClient(self, name, src, dest):
-        if not (name and src and dest):
-            return None
-
-        rules = self.getIptables().get('filter', {}).get('openvpn', {}).get(
-            'rules', [])
-
-        rule = '-A openvpn -i %s -s %s -d %s -m comment --comment "%s" %s' % (
-            'tun0', src, dest, name, '-j ACCEPT'
-        )
-
-        if rule not in rules:
-            self.iptables(rule)
-
-    def flushClient(self, name):
-        rules = self.getIptables().get('filter', {}).get('openvpn', {}).get(
-            'rules', [])
-        for i, r in enumerate(rules):
-            if name in r.split()[-1]:
-                self.iptables('-D openvpn %s' % (i + 1))
 
     def connect(self, user):
         u = User.objects.get(username=user)
