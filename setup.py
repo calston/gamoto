6a6912b35f9ada9af6102be8ede7ebe84c174517
Colin Alston <colin.alston@gmail.com>
2019-07-04 06:37:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'pyopenssl',
-        'django==2.2.2',
+        'django==2.2.3',
         'social-auth-app-django',
         'pyotp',
         'gunicorn',
