6ce27fba0e3d492fbb3a92838aec6c611f19927a
Colin Alston <colin.alston@gmail.com>
2020-11-23 13:45:31 +0000

    wtf django

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'pyopenssl',
-        'django==2.2.17',
+        'Django==2.2.17',
         'social-auth-app-django',
         'pyotp',
         'gunicorn',
